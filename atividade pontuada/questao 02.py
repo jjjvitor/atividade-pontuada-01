80dc5985d54cff86c383a1ea46bfc2133133d028
jjjvitor <jackson.v.santos@ba.estudante.senai.br>
2025-09-08 15:27:01 -0300

    Add files via upload

---
+++ atividade pontuada/questao 02.py
@@ -0,0 +1,39 @@
+
+import os 
+os.system("cls")
+
+print("""
+
+digite o seu sexo
+opcao:
+"m" = masculino or
+"f" = feminino
+
+digite o seu esta civil
+opcao:
+"casado" or "solteiro"
+"casada" or "solteira"
+
+""")
+
+nome = input("digite o seu nome: ")
+sexo = input("digite o seu sexo: ")
+estado_civil = input("digite o seu estado civil: ")
+
+
+
+ 
+" feminino" and estado_civil == "casada"
+int(input("quantos anos de casada? "))
+
+
+
+print("opcao invalida")
+    
+    
+print("fim")
+
+
+
+
+
---
+++ atividade pontuada/questao 04.py
@@ -0,0 +1,12 @@
+import os
+os.system("cls")
+
+
+
+print("""
+      
+Fruta\t	 Até 5 Kg   \t	        Acima de 5 Kg
+Morango\t	 R$ 2,50 por  Kg\t	R$ 2,20 por Kg
+Maçã\t	 R$ 1,80 por  Kg\t	R$ 1,50 por Kg
+
+""")
