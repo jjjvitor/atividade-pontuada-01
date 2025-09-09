4d5ed88efa66c7055116b8bdfb17cc26e0583e9e
jjjvitor <jackson.v.santos@ba.estudante.senai.br>
2025-09-09 12:32:13 -0300

    Add files via upload

---
+++ python/questao 03.py
@@ -0,0 +1,28 @@
+import os
+os.system("cls")
+
+primeiro_valor = int(input("digite o primeiro valor: "))
+segundo_valor = int(input("digite o segundo valor: "))
+
+soma = primeiro_valor + segundo_valor
+multiplicacao = primeiro_valor * segundo_valor
+rtesultado = 0
+
+
+    
+if primeiro_valor == segundo_valor:
+    soma = primeiro_valor + segundo_valor
+    print(f"resultado: {soma}")
+    
+    
+    
+elif multiplicacao == primeiro_valor * segundo_valor:
+    print(f"resultado: {multiplicacao}")
+    
+    
+    
+else:
+     print("opcao invalida")
+
+
+print("hello  world")
